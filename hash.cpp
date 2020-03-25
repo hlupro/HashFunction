93bbfdc67914cf96e3a56966a5a18fb9281eaae3
Hunter <hlupro@gmail.com>
2020-03-25 16:56:24 -0400

    casting sucks

--- hash.cpp
+++ hash.cpp
@@ -33,7 +33,7 @@ size_t hash(std::string str)
   for (int i = 0; i < size; i++)
   {
     ch = str.at(i);
-    x = x + int(ch);
+    x = x + ch;
     std::cout << x << std::endl;
   }
   std::cout << x << std::endl;
